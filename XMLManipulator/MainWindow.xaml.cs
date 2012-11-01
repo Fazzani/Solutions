e03776ba790af0eca594854935b27c36dfe4295e
Fazzani <tunisienheni@outlook.com>
2012-11-01 23:05:52 +0100

    auto disable of controls and save properties

--- XMLManipulator/MainWindow.xaml.cs
+++ XMLManipulator/MainWindow.xaml.cs
@@ -27,6 +27,9 @@ namespace XMLManipulator
     /// </summary>
     public partial class MainWindow
     {
+        /// <summary>
+        /// Provider Univers.Xml
+        /// </summary>
         XmlDataProvider _universProvider;
 
         public MainWindow()
@@ -38,15 +41,16 @@ namespace XMLManipulator
                 this.Closing += MainWindow_Closing;
 
                 Init();
-
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
-
         }
 
+        /// <summary>
+        /// Init App
+        /// </summary>
         void Init()
         {
             if (!string.IsNullOrEmpty(Properties.Settings.Default[ApplicationStaticValues.LastOpenedFilePath].ToString()))
@@ -58,11 +62,15 @@ namespace XMLManipulator
             }
         }
 
+        /// <summary>
+        /// On Closing App
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             if (_universProvider != null && _universProvider.Document != null)
             {
-
                 Properties.Settings.Default[ApplicationStaticValues.LastOpenedFilePath] = new Uri(_universProvider.Document.BaseURI).LocalPath;
                 Properties.Settings.Default.Save();
             }
